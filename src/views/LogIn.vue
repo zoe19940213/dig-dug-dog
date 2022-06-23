1205307a8c87813abc2a98e85396df90889a6870
zoe19940213 <zoe19940213@gmail.com>
2022-06-23 16:11:33 +0800

    LogIn, SignUp page style setting

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="log-in">
-    <h2>Log In</h2>
+    <div class="log-in__form">
+      <div class="input-box">
+        <label for="">帳號 (E-mail)</label>
+        <input type="text" />
+      </div>
+      <div class="input-box">
+        <label for="">密碼</label>
+        <input type="text" />
+      </div>
+      <button class="log-in__form__submit-btn">登入</button>
+      <router-link 
+        to="/signup"
+        class="log-in__form__switch-btn">我要註冊</router-link>
+    </div>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.log-in {
+  position: fixed;
+  top: 100px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  &__form {
+    padding-bottom: 60px;
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    .input-box {
+      margin-top: 2rem;
+      @extend %input-style;
+      input, label{
+        color: $primary-text-color;
+      }
+    }
+    &__submit-btn {
+      margin-top: 2rem;
+      width: 100px;
+      @include set-button($theme-orange, $white-text-color, 20px);
+    }
+    &__switch-btn{
+      margin-top: 1rem;
+      font-size: $secondary-text-size;
+      color: $secondary-text-color;
+      &:hover{
+        opacity: 0.7;
+      }
+    }
+  }
+}
+</style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,5 +1,77 @@
 <template>
   <div class="sign-up">
-    <h2>Sign Up</h2>
+    <div class="sign-up__form">
+      <div class="input-box">
+        <label for="">暱稱</label>
+        <input v-model="name" type="text" />
+      </div>
+      <div class="input-box">
+        <label for="">帳號 (E-mail)</label>
+        <input v-model="account" type="text" />
+      </div>
+      <div class="input-box">
+        <label for="">密碼</label>
+        <input v-model="password" type="text" />
+      </div>
+      <div class="input-box">
+        <label for="">確認密碼</label>
+        <input v-model="passwordCheck" type="text" />
+      </div>
+      <button class="sign-up__form__submit-btn">註冊</button>
+      <router-link 
+        to="/login"
+        class="sign-up__form__switch-btn">我要登入</router-link>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "SignUp",
+  data(){
+    return{
+      name: '',
+      account: '',
+      password: '',
+      passwordCheck: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sign-up {
+  position: fixed;
+  top: 100px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  &__form {
+    padding-bottom: 40px;
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    .input-box {
+      margin-top: 2rem;
+      @extend %input-style;
+      input, label{
+        color: $primary-text-color;
+      }
+    }
+    &__submit-btn {
+      margin-top: 2rem;
+      width: 100px;
+      @include set-button($theme-orange, $white-text-color, 20px);
+    }
+    &__switch-btn{
+      margin-top: 1rem;
+      font-size: $secondary-text-size;
+      color: $secondary-text-color;
+      &:hover{
+        opacity: 0.7;
+      }
+    }
+  }
+}
+</style>
